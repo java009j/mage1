106f8ca96da7d26af52abb3ff154fc227aba4e2c
trama-008 <tramaman003@gmail.com>
2024-08-20 23:58:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,34 +16,34 @@ global.autoreadgc = process.env.AUTOREADGC || true;
 
 
 // Auth information
-global.pairNumber = "919002162806";                         // Add your paining number with country code example "916297175943"; 
+global.pairNumber = "249991672637";                         // Add your paining number with country code example "916297175943"; 
 global.port = process.env.PORT || "10000";
 global.auth = process.env.AUTH || "Pairing";                // Auth mode OR/Pairing.
 global.sessionFile = process.env.SESSION_FILE || "A17-SESSION";
-global.mongodb = process.env.MONGODB || "";                 // Mongodb url.
+global.mongodb = process.env.MONGODB || "mongodb+srv://trama1:trama1@cluster0.0axhygx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";                 // Mongodb url.
 global.website = "https://www.exenoz.tech"; 
-global.github = "https://github.com/Kai0071";
+global.github = "https://github.com/Trama-007";
 
 
 // Default prefix
-global.prefa = process.env.PREFIX ? process.env.PREFIX.split(",") : ["."];
+global.prefa = process.env.PREFIX ? process.env.PREFIX.split("#") : ["."];
 
 
 // Owner information
-global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["916297175943", "919362639362"];
-global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["916297175943", "919362639362"];
-global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["916297175943"];
-global.OwnerName = process.env.OWNER_NAME || "Kai";
-global.BotName = process.env.BOT_NAME || "A17";
-global.packname = process.env.PACK_NAME || "A17 Bot";
-global.author = "By: Kai";
-global.BotSourceCode = "https://github.com/Kai0071/A17";
-global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["249992134574"];
+global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["249992134574"];
+global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["249992134574"];
+global.OwnerName = process.env.OWNER_NAME || "Tramazool";
+global.BotName = process.env.BOT_NAME || "Yuki";
+global.packname = process.env.PACK_NAME || "Yuki Bot";
+global.author = "By: Trama";
+global.BotSourceCode = "https://github.com/trama-008/A17";
+global.SupportGroupLink = "https://chat.whatsapp.com";
 
 
 //
 global.openAiAPI = process.env.OPENAI_API || "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";
-global.location = process.env.LOCATION || "West Bengal, India";
+global.location = process.env.LOCATION || "Sudan , Shendi";
 global.reactmoji = process.env.REACT_MOJI || "❤️";
 global.themeemoji = process.env.THEME_EMOJI || "💖";
 global.vidmenu = { url: process.env.VID_MENU_URL || 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' };
@@ -54,7 +54,7 @@ global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
 global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
 global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
 global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
-global.them = "https://r4.wallpaperflare.com/wallpaper/1003/376/845/makoto-shinkai-kimi-no-na-wa-wallpaper-0816ade8b0301c58302c014e48d2441a.jpg";
+global.them = "https://graph.org/file/5aa9c778c5462118a2e5a.png";
 
 
 //
