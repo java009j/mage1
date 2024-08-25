2dd04dc014d4641ea5b685755275bccfee015c64
trama-008 <tramaman003@gmail.com>
2024-08-25 15:57:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.autoreadgc = process.env.AUTOREADGC || true;
 
 
 // Auth information
-global.pairNumber = "249991672637";                         // Add your paining number with country code example "916297175943"; 
+global.pairNumber = "249916270201";                         // Add your paining number with country code example "916297175943"; 
 global.port = process.env.PORT || "10000";
 global.auth = process.env.AUTH || "Pairing";                // Auth mode OR/Pairing.
 global.sessionFile = process.env.SESSION_FILE || "A17-SESSION";
